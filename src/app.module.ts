5a5e1a1ecb8a3ffe66d96cca62617522c0cdf62f
Rodrigo Cinto <rodcinto@gmail.com>
2023-12-04 17:31:54 +0100

    Integrate configuration, database and users.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { configuration } from '../config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: false,
+      envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
